8a6038507c7d43a047596013d82120f6da594c3b
Peter Ericson <pdericson@gmail.com>
2015-08-27 23:37:08 +1000

    entrypoint.sh: chmod

--- 0.10.0/centos/7/entrypoint.sh
+++ 0.10.0/centos/7/entrypoint.sh
@@ -4,6 +4,8 @@ PRINCIPAL=${PRINCIPAL:-root}
 
 if [ -n "$SECRET" ]; then
     export MARATHON_MESOS_AUTHENTICATION_PRINCIPAL=${MARATHON_MESOS_AUTHENTICATION_PRINCIPAL:-$PRINCIPAL}
+    touch /tmp/secret
+    chmod 600 /tmp/secret
     echo -n "$SECRET" > /tmp/secret
     export MARATHON_MESOS_AUTHENTICATION_SECRET_FILE=/tmp/secret
 fi
